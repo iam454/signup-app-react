40eb006fef256a4e85fb36f6a095a6daebc4c5ba
iam454 <realseogy@gmail.com>
2023-06-06 12:15:25 +0900

    feat: 유효성 검사

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import Form from "./components/Form";
 import Footer from "./components/Footer";
 import FontControlBox from "./components/FontControlBox";
 import Modal from "./components/Modal";
+import { createContext, useState } from "react";
+
+const initialFormData = {
+  id: "",
+  pw: "",
+  confirmPw: "",
+};
+export const FormContext = createContext({
+  formData: initialFormData,
+  setFormData: () => {},
+});
 
 function App() {
+  const [formData, setFormData] = useState(initialFormData);
   return (
-    <>
+    <FormContext.Provider value={{ formData, setFormData }}>
       <section className="form-wrapper">
         <Form />
         <Footer />
       </section>
       <FontControlBox />
       <Modal />
-    </>
+    </FormContext.Provider>
   );
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,12 +1,37 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { FormContext } from "../App";
+
+const ID_REG = new RegExp("^[a-z0-9_-]{5,20}$");
+const PW_REG = new RegExp("^[a-zA-Z0-9]{8,16}$");
 
 const FormInput = ({ id, label, inputProps }) => {
   const inputRef = useRef(null);
+  const { formData, setFormData } = useContext(FormContext);
+
+  const checkReg = () => {
+    const value = formData[id];
+    if (value.length === 0) {
+      return "required";
+    } else {
+      switch (id) {
+        case "id":
+          return ID_REG.test(value) ? true : "invalidId";
+        case "pw":
+          return PW_REG.test(value) ? true : "invalidPw";
+        case "confirmPw":
+          return formData["pw"] === value ? true : "invalidPwCheck";
+        default:
+          return;
+      }
+    }
+  };
+
   useEffect(() => {
     if (id === "id") {
       inputRef.current.focus();
     }
   }, []);
+
   return (
     <div className="mb-4">
       <label
@@ -19,6 +44,9 @@ const FormInput = ({ id, label, inputProps }) => {
         id={id}
         className="shadow border rounded w-full py-2 px-3 text-gray-700"
         ref={inputRef}
+        value={formData[id]}
+        onChange={(e) => setFormData({ ...formData, [id]: e.target.value })}
+        onBlur={checkReg}
         {...inputProps}
       />
       <div className="mt-1 mb-3 text-xs text-red-500"></div>
